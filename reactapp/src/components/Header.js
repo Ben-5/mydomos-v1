7c70f56d6084b0838122386331046eac10744fdc
Benedetto Morici <benmorici@gmail.com>
2020-02-28 17:02:12 +0100

    header style

---
+++ reactapp/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './App.css';
+
+function Header() {
+  return (
+        <header className="header-container">
+          <img src="../logo.png" className="header-logo" alt="logo" />
+          <h1 className="header-title">MYDOMOS</h1>
+        </header>
+    );
+}
+
+export default Header;
