35a58e28f382e1fcf00c2667660760c4443e8d3b
marialena gallizzi <marialena@wanadoo.fr>
2020-03-02 10:58:02 +0100

    button subtitle title composant + css et implementation dans home

---
+++ reactapp/src/screens/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from '../components/Button';
+import Text from '../components/Text';
+import Title from '../components/Title';
+import Subtitle from '../components/Subtitle';
+
+import '../App.css';
+
+
+
+export default function Home(){
+
+
+    return(
+        <div>
+            <p>
+            <Title/>
+            <Subtitle/>
+            <Text/>
+            <Button/>
+            </p>
+        </div>
+
+
+    );
+}
