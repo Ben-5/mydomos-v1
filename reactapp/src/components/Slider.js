ccd98ae3de47ea0e2e79d31280bcd5cf4678d9c5
Sophie Reynaud <sophiereynaud.sr@gmail.com>
2020-03-02 18:42:16 +0100

    slider de cards

--- reactapp/src/components/Slider.js
+++ reactapp/src/components/Slider.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
+import React from 'react'
 import '../index.css';
-import Card from './Card'
 import {Col} from 'antd';
 import {Link} from 'react-router-dom'
 
 export default function Slider(props) {
+
+//ATTENTION
+//Penser à encapsuler les cards du slider dans <div className="scrolling-wrapper"></div>
+
 
     return (
 
@@ -15,9 +18,7 @@ export default function Slider(props) {
             <div className="scrolling-wrapper">
 
                 <Col className="card_col" sm={8} md={6} lg={4}>
-                   
                     <p className="card_info">{props.info}</p>
-                   
                     <Link className="card_link" to={`/visit/${props.id}`}>
                         <img className="card_img" alt="visit cover" src={props.image}/>
                         <p className="card_title">{props.title}</p>
@@ -30,68 +31,7 @@ export default function Slider(props) {
                             <img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/noteG.png'/><img className="slider_rate" alt="note" src='/noteG.png'/>
                         </div>
                     </div>
-               
                 </Col>
-
-            <Col className="card_col" sm={8} md={6} lg={4}>
-                    <p className="card_info">{props.info}</p>
-                    <Link className="card_link" to={`/visit/${props.id}`}>
-                        <img className="card_img" alt="visit cover" src={props.image}/>
-                        <p className="card_title">{props.title}</p>
-                    </Link>
-                    <div className="card_pricerate">
-                        <p className="card_price">À partir de {props.price} €</p>
-                        <div className="card_div_rate">
-                            <img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/noteG.png'/><img className="slider_rate" alt="note" src='/noteG.png'/>
-                        </div>
-                    </div>
-               
-            </Col>
-
-            <Col className="card_col" sm={8} md={6} lg={4}>
-                    <p className="card_info">{props.info}</p>
-                    <Link className="card_link" to={`/visit/${props.id}`}>
-                        <img className="card_img" alt="visit cover" src={props.image}/>
-                        <p className="card_title">{props.title}</p>
-                    </Link>
-                    <div className="card_pricerate">
-                        <p className="card_price">À partir de {props.price} €</p>
-                        <div className="card_div_rate">
-                            <img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/noteG.png'/><img className="slider_rate" alt="note" src='/noteG.png'/>
-                        </div>
-                    </div>
-               
-            </Col>
-
-            <Col className="card_col" sm={8} md={6} lg={4}>
-                    <p className="card_info">{props.info}</p>
-                    <Link className="card_link" to={`/visit/${props.id}`}>
-                        <img className="card_img" alt="visit cover" src={props.image}/>
-                        <p className="card_title">{props.title}</p>
-                    </Link>
-                    <div className="card_pricerate">
-                        <p className="card_price">À partir de {props.price} €</p>
-                        <div className="card_div_rate">
-                            <img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/noteG.png'/><img className="slider_rate" alt="note" src='/noteG.png'/>
-                        </div>
-                    </div>
-               
-            </Col>
-            <Col className="card_col" sm={8} md={6} lg={4}>
-                    <p className="card_info">{props.info}</p>
-                    <Link className="card_link" to={`/visit/${props.id}`}>
-                        <img className="card_img" alt="visit cover" src={props.image}/>
-                        <p className="card_title">{props.title}</p>
-                    </Link>
-                    <div className="card_pricerate">
-                        <p className="card_price">À partir de {props.price} €</p>
-                        <div className="card_div_rate">
-                            <img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/note.png'/><img className="slider_rate" alt="note" src='/noteG.png'/><img className="slider_rate" alt="note" src='/noteG.png'/>
-                        </div>
-                    </div>
-               
-            </Col>
-       
 
             </div>
 
