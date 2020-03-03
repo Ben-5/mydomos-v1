cec3f4c7df6a4e1ae0548b12730e13a80746d720
Benedetto Morici <benmorici@gmail.com>
2020-03-03 15:15:11 +0100

    home

--- reactapp/src/components/Footer.js
+++ reactapp/src/components/Footer.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import Input from '../components/input';
+import OkButton from '../components/OkButton';
+import Title from '../components/Title';
+import Subtitle from '../components/Subtitle';
 
 export default function Footer() {
     return (
-
-        <footer className= "footer-container">
+    <footer>
+        <div className="newsletter-container">
+            <div className="newsletter">
+                <Title title="Soyez exclusifs !"/>
+                <Subtitle subtitle="N'attendez plus ! Abbonez-vous à notre newsletter pour découvrir les nouvelles visites et offres exclusives en priorité !"/>
+                <div className="newsletter-form">
+                    <Input/>
+                    <OkButton/>
+                </div>
+            </div>
+            <div className="legs-container">
+                <img src="../legs.png" className="newsletter-legs" alt="legs" />  
+            </div>
+        </div>    
+        <div className= "footer-container">
             <div className= "socials-container">
                 <img src="../facebook.png" className="socials-button" alt="facebook" />
                 <img src="../instagram.png" className="socials-button" alt="instagram" />
@@ -19,7 +35,7 @@ export default function Footer() {
                 <img src="../sleeping.png" className="sleep" alt="sleep" />
                 <h4 className="footer-title">© MYDOMOS</h4>
             </div>
-        </footer>
-
+        </div>
+    </footer>
         );
     }
